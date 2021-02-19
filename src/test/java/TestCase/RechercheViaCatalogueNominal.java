98ef675fdc7161329e1cbd394d276746e3005abf
Helene-B17 <helene.beuchee@gmail.com>
2021-02-19 14:26:44 +0100

    correction nominal

--- src/test/java/TestCase/RechercheViaCatalogueNominal.java
+++ src/test/java/TestCase/RechercheViaCatalogueNominal.java
@@ -11,62 +11,38 @@ import cucumber.api.java.en.When;
 
 public class RechercheViaCatalogueNominal {
 
-	@Given("En tant qu utilisateur je souhaite pouvoir rechercher un produit de fa�on � pouvoir afficher le d�tail du produit via le catalogue")
-	public void en_tant_qu_utilisateur_je_souhaite_pouvoir_rechercher_un_produit_de_fa_on_pouvoir_afficher_le_d_tail_du_produit_via_le_catalogue() {
+	@Given("En tant qu utilisateur je souhaite pouvoir rechercher un produit de facon a pouvoir afficher le datail du produit via le catalogue")
+	public void en_tant_qu_utilisateur_je_souhaite_pouvoir_rechercher_un_produit_de_facon_a_pouvoir_afficher_le_datail_du_produit_via_le_catalogue() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
 
-	@When("Je veux naviguer dans le {string}")
-	public void je_veux_naviguer_dans_le(String string) {
+	@When("Je veux naviguer dans le {string} puis s�lectionner une {string} afin de s�lectionner un {string}")
+	public void je_veux_naviguer_dans_le_puis_s�lectionner_une_afin_de_s�lectionner_un(String string, String string2, String string3) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
 
-	@When("Je veux sélectionner une {string}")
-	public void je_veux_sélectionner_une(String string) {
+	@When("Je veux voir le {string} sur produit pour l ajouter au panier")
+	public void je_veux_voir_le_sur_produit_pour_l_ajouter_au_panier(String string) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
 
-	@When("Je veux s�lectionner un {string}")
-	public void je_veux_s_lectionner_un(String string) {
+	@When("je verifie que {string} est ajoute au panier et que la {int} est la bonne ainsi que le {string}")
+	public void je_verifie_que_est_ajoute_au_panier_et_que_la_est_la_bonne_ainsi_que_le(String string, Integer int1, String string2) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
 
-	@When("Je veux voir le {string} sur produit")
-	public void je_veux_voir_le_sur_produit(String string) {
+	@When("Les verifications sont terminees je souhaite retirer {int}")
+	public void les_verifications_sont_terminees_je_souhaite_retirer(Integer int1) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
 
-	@When("Je verifie que {string} est ajout� au panier")
-	public void je_verifie_que_est_ajout_au_panier(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@When("Que la {int} est la bonne")
-	public void que_la_est_la_bonne(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("Que le {string} est le bon")
-	public void que_le_est_le_bon(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@When("Les vérifications sont terminées je souhaite retirer {int}")
-	public void les_vérifications_sont_terminées_je_souhaite_retirer(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@When("Je vérifie que le message {string} est présent")
-	public void je_vérifie_que_le_message_est_présent(String string) {
+	@Then("Je verifie le message {string} est present")
+	public void je_verifie_le_message_est_present(String string) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}

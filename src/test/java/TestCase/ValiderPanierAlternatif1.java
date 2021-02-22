21e80f9fa4beb8087e1cdc25084062ef991cc167
sarah1202 <sarahetarnold@gmail.com>
2021-02-22 11:02:58 +0100

    ValiderPanierAlternatif1 feature+java

---
+++ src/test/java/TestCase/ValiderPanierAlternatif1.java
@@ -0,0 +1,45 @@
+package TestCase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ValiderPanierAlternatif1 {
+
+	@Given("En tant qu utilisateur je souhaite afficher mon panier de facon a pouvoir le modifier avant de le valider")
+	public void en_tant_qu_utilisateur_je_souhaite_afficher_mon_panier_de_facon_a_pouvoir_le_modifier_avant_de_le_valider() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je verifie {string}, {int}, {string} dans le panier ainsi que le {string} du panier")
+	public void je_verifie_dans_le_panier_ainsi_que_le_du_panier(String string, Integer int1, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je veux modifier la {int} du {string}")
+	public void je_veux_modifier_la_du(Integer int1, String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur Actualiser mon panier")
+	public void je_clique_sur_Actualiser_mon_panier() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("je verifie la {int}, {string}, {string} et le {string} dans le contenu du panier.")
+	public void je_verifie_la_et_le_dans_le_contenu_du_panier(Integer int1, String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}

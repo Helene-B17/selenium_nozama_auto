44a92790a9c718113e26a4883078b5d6e83bd83b
Helene-B17 <helene.beuchee@gmail.com>
2021-02-22 13:56:55 +0100

    ValiderCommande cucumber et java

---
+++ Features/ValiderCommande.feature
@@ -0,0 +1,19 @@
+Feature: Valider commande nominal
+Scenario Outline: L utilisateur a valide son panier et remplit les champs afin de valider sa commande
+	Given je suis sur la page avec le contenu de mon panier
+	When je valide le panier et le systeme me recapitule le <produit>, sa <quantite> et son <prix>
+	And le systeme me demande de m authentifier avec mon <login> et <motdepasse>
+	And je clique sur "suivant" et je remplis les champs du bon de commande avec mon <prenom>, <nom>, <adresse>, <ville>, <pays> et mon <codePostal>
+	And je clique sur "suivant" et je coche l option "Mon adresse de facturation est identique à mon adresse de livraison."
+	And je clique sur "suivant" et je verifie le montant du <colisPostal>
+	And je clique sur "suivant" et verifie le <prix>, le <colisPostal>, le <montantTTC> et le <totalCommande>
+	And je remplis les champs du mode de paiement avec le <numCarte>, <dateExp> et <crypto>
+	And je clique sur "suivant" puis sur "vérifier la commande"
+	And une page recapitulant les infos <produit>, <quantite>, <prix>, <prenom>, <nom>, <adresse>, <codePostal>, <ville>, <colisPostal>, <totalCommande>, les quatre derniers chiffres du <numCarte> et la <dateExp> s affiche
+	And je clique sur "Submit order"
+	Then le message "Commande terminée" s affiche avec le numero de la commande.
+	
+		Examples:
+		| produit							| quantite 		| prix			| login 	| motdepasse 	| prenom 		| nom 		| adresse 	| ville 	| pays 			| codePostal 	| colisPostal 					| montantTTC 	| totalCommande | numCarte 						| dateExp			 			| crypto 	|
+		| "Nintendo DS Lite"	|  "1"				| "145,80"	| "demo1" | "demo1" 		| "demo" 		| "demo" 	| "demo" 		| "demo" 	| "France" 	| "35000" 		| "Colis postal: 8,00 �" 	| "174,96" 		| "182,96"			| "5181888346131442" 	| "03 - March 2021" | "111" 	|
+		| "Test logiciel"			|  "2"				| "72,00"		| "demo1" | "demo1" 		| "demo" 		| "demo" 	| "demo" 		| "demo" 	| "France" 	| "35000" 		| "Colis postal: 8,00 �" 	| "75,96" 		| "83,96"				| "5181888346131442" 	| "03 - March 2021" | "111" 	|
---
+++ src/test/java/TestCase/ValiderCommande.java
@@ -0,0 +1,87 @@
+package TestCase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ValiderCommande {
+
+	@Given("je suis sur la page avec le contenu de mon panier")
+	public void je_suis_sur_la_page_avec_le_contenu_de_mon_panier() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je valide le panier et le systeme me recapitule le {string}, sa {string} et son {string}")
+	public void je_valide_le_panier_et_le_systeme_me_recapitule_le_sa_et_son(String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("le systeme me demande de m authentifier avec mon {string} et {string}")
+	public void le_systeme_me_demande_de_m_authentifier_avec_mon_et(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur {string} et je remplis les champs du bon de commande avec mon {string}, {string}, {string}, {string}, {string} et mon {string}")
+	public void je_clique_sur_et_je_remplis_les_champs_du_bon_de_commande_avec_mon_et_mon(String string, String string2, String string3, String string4, String string5, String string6, String string7) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur {string} et je coche l option {string}")
+	public void je_clique_sur_et_je_coche_l_option(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur {string} et je verifie le montant du {string}")
+	public void je_clique_sur_et_je_verifie_le_montant_du(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur {string} et verifie le {string}, le {string}, le {string} et le {string}")
+	public void je_clique_sur_et_verifie_le_le_le_et_le(String string, String string2, String string3, String string4, String string5) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je remplis les champs du mode de paiement avec le {string}, {string} et {string}")
+	public void je_remplis_les_champs_du_mode_de_paiement_avec_le_et(String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur {string} puis sur {string}")
+	public void je_clique_sur_puis_sur(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("une page recapitulant les infos {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, les quatre derniers chiffres du {string} et la {string} s affiche")
+	public void une_page_recapitulant_les_infos_les_quatre_derniers_chiffres_du_et_la_s_affiche(String string, String string2, String string3, String string4, String string5, String string6, String string7, String string8, String string9, String string10, String string11, String string12) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("je clique sur {string}")
+	public void je_clique_sur(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("le message {string} s affiche avec le numero de la commande.")
+	public void le_message_s_affiche_avec_le_numero_de_la_commande(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
